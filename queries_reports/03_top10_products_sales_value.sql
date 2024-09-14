1b0e14a8618e1a179955665dc5f3a140dc543b7c
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-09-14 12:19:27 -0300

    Updating: Ranking the products

--- queries_reports/03_top10_products_sales_value.sql
+++ queries_reports/03_top10_products_sales_value.sql
@@ -1,9 +1,9 @@
 SELECT 
 	product_name,
+	RANK() OVER (ORDER BY SUM(order_value) DESC) AS ranking,
 	ROUND(SUM(order_value)::numeric, 2) AS sales_value
 FROM temp_table_orders_values
 JOIN products
 	USING(product_id)
 GROUP BY product_name
-ORDER BY sales_value DESC
 LIMIT 10;
